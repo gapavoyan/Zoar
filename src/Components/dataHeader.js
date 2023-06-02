cbb1c4b8968407d5e05995e3ca368472d89e5783
gapavoyan <gap_avoyan@mail.ru>
2023-06-02 17:42:12 +0400

    created a header using a database

---
+++ src/Components/dataHeader.js
@@ -0,0 +1,22 @@
+export const dataHeader = [
+    {
+        id:1,
+        nav_items:"ՇԵՆՔԻ ԿԱՌՈՒՑՎԱԾՔԸ"
+    },
+    {
+        id:2,
+        nav_items:"ՇԵՆՔԻ ՆԿԱՐԱԳՐՈՒԹՅՈՒՆ"
+    },
+    {
+        id:3,
+        nav_items:"ՓԱԹԵԹՆԵՐ"
+    },
+    {
+        id:4,
+        nav_items:"ՀԱՏԱԿԱԳԾԵՐ"
+    },
+    {
+        id:5,
+        nav_items:"ԿԱՊ"
+    }
+]
--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { dataHeader } from './dataHeader'
 function Header() {
+    const[state,setState] = useState([])
+    useEffect(()=>{
+        setState(dataHeader)
+    },[])
     return (
         <div>
             <header className=' h-[150px] flex items-center font-montserrat-arm'>
                 <div>
                     <img src='https://zoar.am/images/zoar-header-logo.svg' className='w-[102px] h-[88px] absolute bottom-[98.1%] left-[160px] top-[32px] right-[88.39%]' />
                 </div>
-                <div className='absolute left-[402px] w-[938px] h-[20px] '>
-                    <nav className='flex gap-[83px] text-[#AFAEB1] leading-[20px] '>
-                        <ul className='flex gap-[43px] ' >
-                            <li className='hover:text-[#0073B6]'>ՇԵՆՔԻ ԿԱՌՈՒՑՎԱԾՔԸ</li>
-                            <li className='hover:text-[#0073B6]'>ՇԵՆՔԻ ՆԿԱՐԱԳՐՈՒԹՅՈՒՆ</li>
-                        </ul>
-                        <ul className='flex gap-[52px]'>
-                            <li className='hover:text-[#0073B6]'>ՓԱԹԵԹՆԵՐ</li>
-                            <li className='hover:text-[#0073B6]'>ՀԱՏԱԿԱԳԾԵՐ</li>
-                            <li className='hover:text-[#0073B6]'>ԿԱՊ</li>
+                <div className='absolute left-[402px]'>
+                    <nav>
+                        <ul className='flex gap-[83px] ' >
+                            {
+                                state.map((el) => <li key={el.id} className='hover:text-[#0073B6] text-[#AFAEB1] leading-[20px] '>{el.nav_items}</li>)
+                            }
                         </ul>
                     </nav>
                 </div>
